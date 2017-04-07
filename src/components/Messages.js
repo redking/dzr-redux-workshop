91dc063bbfb2a457595cf15e75855a1bf88f0859
Cormac Flynn <cf@deezer.com>
2017-04-07 17:25:46 +0200

    Removing unused code

--- src/components/Messages.js
+++ src/components/Messages.js
@@ -6,14 +6,14 @@ import Message from './Message';
 class Messages extends Component {
 
 	render() {
-		const {messages, onMessageDelete} = this.props;
+		const {messages} = this.props;
 
 		return (
 			<ol className="room list-unstyled">
 				{!messages.length ? <li><i className="fa fa-exclamation-triangle" /> No messages :'(</li> : null}
 				{messages.map((message, i) =>
 					<li key={'message_' + i}>
-						<Message {...message} index={i} onMessageDelete={onMessageDelete} />
+						<Message {...message} />
 					</li>
 				)}
 			</ol>
